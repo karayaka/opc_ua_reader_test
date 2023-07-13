bad15de4bbd2c8d04729f439fe4128a0f55a45ed
Çağrı Karayaka <cagri.karayaka@hotmail.com>
2023-07-13 17:17:29 +0300

    first commit

---
+++ views/JobInfoForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace opc_ua_reader_test.views
+{
+    public partial class JobInfoForm : Form
+    {
+        public JobInfoForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
