4dc0c362a3e62b49b759d53e15410a246ac6906e
Çağrı Karayaka <cagri.karayaka@hotmail.com>
2023-07-13 17:50:09 +0300

    commit for models

--- views/JobInfoForm.Designer.cs
+++ views/JobInfoForm.Designer.cs
@@ -29,15 +29,74 @@
         private void InitializeComponent()
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.txtJobGuid = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtJobGuid
+            // 
+            this.txtJobGuid.Location = new System.Drawing.Point(74, 12);
+            this.txtJobGuid.Name = "txtJobGuid";
+            this.txtJobGuid.Size = new System.Drawing.Size(824, 22);
+            this.txtJobGuid.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "JobGuid:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(904, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Read Data";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtJobGuid);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1081, 47);
+            this.panel1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1081, 403);
+            this.dataGridView1.TabIndex = 4;
             // 
             // JobInfoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1081, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
             this.Name = "JobInfoForm";
             this.Text = "JobInfoForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -45,5 +104,10 @@
         #endregion
 
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox txtJobGuid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- views/JobInfoForm.cs
+++ views/JobInfoForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using opc_ua_reader_test.Models;
+using opc_ua_reader_test.Repositorys;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,8 +19,18 @@ namespace opc_ua_reader_test.views
         {
             InitializeComponent();
         }
+        List<JobInfoModel> jobInfo = new List<JobInfoModel>();
+        public void rederJobınfo() {
 
-        public void rederJobınfo() { }
+            var objs = OpcReaderRepository.Instance.CallMethodts(
+                    opcUrl,
+                    "ns=2;s=History",
+                    "ns=2;s=History.GetJobInfo",
+                    jobInfoGuid
+                );
+            jobInfo = JsonConvert.DeserializeObject<List<JobInfoModel>>(objs[0].ToString());
+
+        }
 
 
     }
